df6182a6d4fa99ecaeb2bcf6572917f1257bd7dd
Ryan Frazier <frazierr2@gmail.com>
2016-10-18 15:46:35 -0400

    Project 2/3 done

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,3 +1,24 @@
 var _ = require('underscore');
 var $ = require('jquery');
 var Backbone = require('backbone');
+var SubmitButtonCollection = require('./models/submit').SubmitButtonCollection;
+
+
+var loadingFetch = new SubmitButtonCollection();
+// loadingFetch.fetch();
+// console.log(loadingFetch);
+loadingFetch.on('request', function() {
+  $('.submit').text('Loading...');
+});
+
+loadingFetch.on('sync', function() {
+  console.log(loadingFetch);
+  setTimeout(function() {
+    $('.submit').text('Submit');
+  }, 1000);
+});
+
+$('#loading-button').on('click', function() {
+  loadingFetch.fetch()
+
+});
--- app/scripts/models/submit.js
+++ app/scripts/models/submit.js
@@ -1,3 +1,20 @@
 var _ = require('underscore');
 var $ = require('jquery');
 var Backbone = require('backbone');
+
+var SubmitButton = Backbone.Model.extend();
+
+var SubmitButtonCollection = Backbone.Collection.extend({
+  model: SubmitButton,
+  url: 'http://tiny-lasagna-server.herokuapp.com/collections/posts',
+  parse: function(data) {
+    return data;
+  }
+
+});
+
+
+module.exports = {
+  SubmitButton: SubmitButton,
+  SubmitButtonCollection: SubmitButtonCollection
+}
