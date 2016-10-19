cde29c9186de38cec628db194779d97e96979cff
Ryan Frazier <frazierr2@gmail.com>
2016-10-18 22:21:23 -0400

    added a form to the document

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -7,23 +7,38 @@ var SubmitButtonCollection = require('./models/submit').SubmitButtonCollection;
 var loadingFetch = new SubmitButtonCollection();
 // loadingFetch.fetch();
 // console.log(loadingFetch);
+
+//****SETTING UP THE LOADING ONCE PUSHED FEATURE******
 loadingFetch.on('request', function() {
   $('.submit').text('Loading...');
 });
-
+//******SETTING UP THE FETCHING DATA FROM API ******
 loadingFetch.on('sync', function() {
   console.log(loadingFetch);
   setTimeout(function() {
     $('.submit').text('Submit');
   }, 1000);
 });
-
+//****POSTING RESULTS TO THE DOM*****
 $('.blog-post').append('<ul></ul>');
 loadingFetch.on("add", function(data) {
   $('.blog-post ul').append('<li>' + data.get('title') + '</li>');
 });
-
-$('#loading-button').on('click', function() {
+//*****FETCHING THE DATA ONCE THE BUTTON IS PUSHED*****
+$('#loading-button').on('click', function(e) {
+  e.preventDefault();
   loadingFetch.fetch()
 
 });
+
+//***CREATING FORM TO POST TO API SERVER*****
+$('#blog-submit').on('click', function(e) {
+  e.preventDefault();
+
+  loadingFetch.create({
+    title: $('#blog-title').val(),
+    blog: $('#blog-post-box').val()
+  });
+  $('#blog-title').val('');
+  $('#blog-post-box').val('');
+})
